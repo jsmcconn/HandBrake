00d234207a986c95ac364be08506515efc36472c
Jeremiah McConnell <miah@miah.com>
2018-06-07 12:56:08 -0600

    libhb: Cleanup debug code

--- libhb/decpgssub.c
+++ libhb/decpgssub.c
@@ -72,7 +72,7 @@ static int decsubInit( hb_work_object_t * w, hb_job_t * job )
     return 0;
 }
 
-static int crop_pgs( hb_buffer_t * buf, hb_work_private_t * pv, int commit_changes )
+static int crop_pgs( hb_buffer_t * buf, hb_work_private_t * pv)
 {
     hb_buffer_t * b = buf;
     int success = 1;
@@ -126,10 +126,7 @@ static int crop_pgs( hb_buffer_t * buf, hb_work_private_t * pv, int commit_chang
                 uint16_t video_width, video_height;
                 uint8_t num_composition_objects;
 
-                // if (!commit_changes)
-                // {
-                //     hb_log("[PGS Crop] Found Presentation Composition Segment");
-                // }
+                // hb_log("[PGS Crop] Found Presentation Composition Segment");
 
                 // Set the video width/height based on the job
                 video_width = ((int)b->data[jj+0] << 8) + b->data[jj+1];
@@ -137,17 +134,15 @@ static int crop_pgs( hb_buffer_t * buf, hb_work_private_t * pv, int commit_chang
 
                 if (video_width < pv->job->width || video_height < pv->job->height)
                 {
-                    hb_log("[warning] PGS subtitle PCS found with dimensions %Xx%X", video_width, video_height);
+                    hb_log("[warning] PGS subtitle PCS found with dimensions %dx%d, larger than job size %dx%d",
+                        video_width, video_height, pv->job->width, pv->job->height);
                     success = 0;
                 }
 
-                if ( commit_changes )
-                {
-                    b->data[jj+0] = pv->job->width >> 8;
-                    b->data[jj+1] = pv->job->width & 0xFF;
-                    b->data[jj+2] = pv->job->height >> 8;
-                    b->data[jj+3] = pv->job->height & 0xFF;
-                }
+                b->data[jj+0] = pv->job->width >> 8;
+                b->data[jj+1] = pv->job->width & 0xFF;
+                b->data[jj+2] = pv->job->height >> 8;
+                b->data[jj+3] = pv->job->height & 0xFF;
 
                 num_composition_objects = b->data[jj+10];
                 jj += 11;
@@ -163,7 +158,7 @@ static int crop_pgs( hb_buffer_t * buf, hb_work_private_t * pv, int commit_chang
                     //     uint16 object_vertical_position;
                     //
                     //     // The following are only present if
-                    //     // object_cropped_flag == 0x40
+                    //     // object_cropped_flag == 0x80 is set
                     //     uint16 object_cropping_horizontal_position;
                     //     uint16 object_cropping_vertical_position;
                     //     uint16 object_cropping_width;
@@ -179,13 +174,10 @@ static int crop_pgs( hb_buffer_t * buf, hb_work_private_t * pv, int commit_chang
                     object_horizontal_position = ((int)b->data[jj+4] << 8) + b->data[jj+5];
                     object_vertical_position = ((int)b->data[jj+6] << 8) + b->data[jj+7];
 
-                    if ( commit_changes )
-                    {
-                        b->data[jj+4] = (object_horizontal_position - pv->job->crop[2]) >> 8;
-                        b->data[jj+5] = (object_horizontal_position - pv->job->crop[2]) & 0xFF;
-                        b->data[jj+6] = (object_vertical_position - pv->job->crop[0]) >> 8;
-                        b->data[jj+7] = (object_vertical_position - pv->job->crop[0]) & 0xFF;
-                    }
+                    b->data[jj+4] = (object_horizontal_position - pv->job->crop[2]) >> 8;
+                    b->data[jj+5] = (object_horizontal_position - pv->job->crop[2]) & 0xFF;
+                    b->data[jj+6] = (object_vertical_position - pv->job->crop[0]) >> 8;
+                    b->data[jj+7] = (object_vertical_position - pv->job->crop[0]) & 0xFF;
 
                     if (object_cropped_flag & 0x80 && jj + 16 <= b->size)
                     {
@@ -203,19 +195,16 @@ static int crop_pgs( hb_buffer_t * buf, hb_work_private_t * pv, int commit_chang
                         if (object_cropping_horizontal_position - pv->job->crop[2] + object_cropping_width > pv->job->width || 
                             object_cropping_vertical_position  - pv->job->crop[0] + object_cropping_height > pv->job->height)
                         {
-                            hb_log("[warning] PGS cropped object exceeds viewport [%d posx, %d posy, %d sizex, %d sizey]",
+                            hb_log("[warning] PGS cropped object bounds exceeds viewport [%d posx, %d posy, %d sizex, %d sizey]",
                                 object_cropping_horizontal_position, object_cropping_vertical_position,
                                 object_cropping_width, object_cropping_height);
                             success = 0;
                         }
 
-                        if ( commit_changes )
-                        {
-                            b->data[jj+8] = (object_cropping_horizontal_position - pv->job->crop[2]) >> 8;
-                            b->data[jj+9] = (object_cropping_horizontal_position - pv->job->crop[2]) & 0xFF;
-                            b->data[jj+10] = (object_cropping_vertical_position - pv->job->crop[0]) >> 8;
-                            b->data[jj+11] = (object_cropping_vertical_position - pv->job->crop[0]) & 0xFF;
-                        }
+                        b->data[jj+8] = (object_cropping_horizontal_position - pv->job->crop[2]) >> 8;
+                        b->data[jj+9] = (object_cropping_horizontal_position - pv->job->crop[2]) & 0xFF;
+                        b->data[jj+10] = (object_cropping_vertical_position - pv->job->crop[0]) >> 8;
+                        b->data[jj+11] = (object_cropping_vertical_position - pv->job->crop[0]) & 0xFF;
 
                         jj += 8;
                     }
@@ -235,10 +224,7 @@ static int crop_pgs( hb_buffer_t * buf, hb_work_private_t * pv, int commit_chang
                 int kk, jj = ii;
                 uint8_t num_window_definition_objects;
 
-                // if (!commit_changes)
-                // {
-                //     hb_log("[PGS Crop] Found Window Definition Segment");
-                // }
+                // hb_log("[PGS Crop] Found Window Definition Segment");
 
                 num_window_definition_objects = b->data[jj+0];
 
@@ -255,8 +241,8 @@ static int crop_pgs( hb_buffer_t * buf, hb_work_private_t * pv, int commit_chang
                     //     uint16 window_height;
                     // }
                     //
-                    // In this block we want to change the yPosition to reflect
-                    // the job y crop.
+                    // In this block we want to change the position to reflect
+                    // the job crop.
 
                     uint16_t window_horizontal_position, window_vertical_position,
                         window_width, window_height;
@@ -275,34 +261,31 @@ static int crop_pgs( hb_buffer_t * buf, hb_work_private_t * pv, int commit_chang
                         success = 0;
                     }
 
-                    if ( commit_changes )
-                    {
-                        b->data[jj+1] = (window_horizontal_position - pv->job->crop[2]) >> 8;
-                        b->data[jj+2] = (window_horizontal_position - pv->job->crop[2]) & 0xFF;
-                        b->data[jj+3] = (window_vertical_position - pv->job->crop[0]) >> 8;
-                        b->data[jj+4] = (window_vertical_position - pv->job->crop[0]) & 0xFF;
-                    }
+                    b->data[jj+1] = (window_horizontal_position - pv->job->crop[2]) >> 8;
+                    b->data[jj+2] = (window_horizontal_position - pv->job->crop[2]) & 0xFF;
+                    b->data[jj+3] = (window_vertical_position - pv->job->crop[0]) >> 8;
+                    b->data[jj+4] = (window_vertical_position - pv->job->crop[0]) & 0xFF;
 
                     jj += 9;
                     kk += 1;
                 }
             }
-            // else if (segment_type == 0x14 && !commit_changes)
-            // {
-            //     hb_log("[PGS Crop] Found Palette Definition Segment");
-            // }
-            // else if (segment_type == 0x15 && !commit_changes)
-            // {
-            //     hb_log("[PGS Crop] Found Object Definition Segment");
-            // }
-            // else if (segment_type == 0x80 && !commit_changes)
-            // {
-            //     hb_log("[PGS Crop] Found End of Display Set Segment");
-            // }
-            // else if (!commit_changes)
-            // {
-            //     hb_log("[PGS Crop] Found Unknown Segment");
-            // }
+            else if (segment_type == 0x14)
+            {
+                // hb_log("[PGS Crop] Found Palette Definition Segment");
+            }
+            else if (segment_type == 0x15)
+            {
+                // hb_log("[PGS Crop] Found Object Definition Segment");
+            }
+            else if (segment_type == 0x80)
+            {
+                // hb_log("[PGS Crop] Found End of Display Set Segment");
+            }
+            else
+            {
+                hb_log("[PGS Crop] Found Unknown Segment");
+            }
             ii += segment_size;
         }
         b = b->next;
@@ -581,16 +564,12 @@ static int decsubWork( hb_work_object_t * w, hb_buffer_t ** buf_in,
                  * merge them. */
 
                 // Update the pgs stream to reflect the job image crop
-                // hb_log("[PGS Crop] Validating PGS subtitle stream crop");
-                if ( crop_pgs(hb_buffer_list_head(&pv->list_pass), pv, 0) )
+                //hb_log("[PGS Crop] Cropping PGS subtitle stream at %ds (pts %"PRId64")", pts / (90 * 1000), pts);
+                if ( crop_pgs(hb_buffer_list_head(&pv->list_pass), pv) != 1 )
                 {
-                    //hb_log("[PGS Crop] Cropping PGS subtitle stream at %ds (pts %"PRId64")", pts / (90 * 1000), pts);
-                    crop_pgs(hb_buffer_list_head(&pv->list_pass), pv, 1);
+                    hb_log("[warning] PGS subtitle cropping reported errors for stream at %"PRId64" s (pts %"PRId64")", pts / (90 * 1000), pts);
                 }
-                else
-                {
-                    hb_log("[warning] PGS subtitle cropping disabled for stream at %ds (pts %"PRId64")", pts / (90 * 1000), pts);
-                }
+
 
                 if (hb_buffer_list_count(&pv->list_pass) == 1)
                 {
